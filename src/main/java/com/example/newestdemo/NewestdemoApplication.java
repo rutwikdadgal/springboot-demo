6848d0d225291316b237df4c7d220ff8c18726d9
Rutwik <rutwikdadgal2001@gmail.com>
2023-11-16 09:44:38 +0530

    project committed

---
+++ src/main/java/com/example/newestdemo/NewestdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.newestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewestdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewestdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/newestdemo/NewestdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newestdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewestdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
